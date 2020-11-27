0795181cc96e890f99f286bba8c28364dafc3c7d
elie roure <elie.roure@etu.umontpellier>
2020-11-27 14:30:26 +0100

    first push elie

--- view/view.php
+++ view/view.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title><?php echo $pagetitle; ?></title>
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $pagetitle; ?></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
     <nav style="border-bottom: 2px grey solid">
         <ul>
-            
+
             <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll > Accueil</a></li>
             <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll> Tout les utilisateurs (admin)</a></li>
 
 
-            </ul>
-            </nav>
-            </head>
-            <body>
-            <?php
+        </ul>
+    </nav>
+</head>
+<body>
+    <?php
 // Si $controleur='voiture' et $view='list',
 // alors $filepath="/chemin_du_site/view/voiture/list.php"
 
-            $filepath = File::build_path(array("view", $controller, "$view.php"));
-            require $filepath;
-            ?>
-            </body>
-            <footer>
-                <p style="border: 1px solid black;text-align:right;padding-right:1em;">
-                    Site vente de pierres de Elie Roure, Etienne Tillier et Guilhem Cros
-                </p>
-            </footer>
-            </html>
+    $filepath = File::build_path(array("view", $controller, "$view.php"));
+    require $filepath;
+    ?>
+</body>
+<footer>
+    <p style="border: 1px solid black;text-align:right;padding-right:1em;">
+        Site vente de pierres de Elie Roure, Etienne Tillier et Guilhem Cros
+    </p>
+</footer>
+</html>
 
