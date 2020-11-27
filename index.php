477f08a0705c8b22e2a81d4faa689a56847cecb6
Etienne <etienne.tillier@etu.umontpellier.fr>
2020-11-27 16:15:20 +0100

    ajout require once session

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
         $DS = DIRECTORY_SEPARATOR;
         require_once "{$ROOT_FOLDER}$DS". "lib" . "$DS" . "File.php";
         require_once File::build_path(array("controller", "routeur.php"));
+        require_once $ROOT_FOLDER . $DS.join($DS, array('lib','Session.php'));
         ?>
 
