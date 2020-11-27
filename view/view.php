2ee72036847148689f1d25f845ccdb67fcead1de
guilhem-cros <guilhem.cros01@etu.umontpellier.fr>
2020-11-27 14:22:50 +0100

    modif view php

--- view/view.php
+++ view/view.php
@@ -6,9 +6,8 @@
     <nav style="border-bottom: 2px grey solid">
         <ul>
             
-            <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll > Toutes nos pierres</a></li>
-            <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll&controller=type> Par type</a></li>
-            <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll&controller=forme> Par forme</a></li>
+            <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll > Accueil</a></li>
+            <li style="display:inline; margin-right:1em; padding: 3px; border:2px black solid"><a href = index.php?action=readAll> Tout les utilisateurs (admin)</a></li>
 
 
             </ul>
@@ -25,7 +24,7 @@
             </body>
             <footer>
                 <p style="border: 1px solid black;text-align:right;padding-right:1em;">
-                    Site vente de pierres de Elie Roure, Etienne Tillier et Guilhem Cros (le plus bo des 3)
+                    Site vente de pierres de Elie Roure, Etienne Tillier et Guilhem Cros
                 </p>
             </footer>
             </html>
