9f39c5761e87bb8601288675dc509193b49f3d02
elie roure <elie.roure@etu.umontpellier>
2020-11-30 10:36:22 +0100

    gestion img

--- view/view.php
+++ view/view.php
@@ -34,6 +34,7 @@
 // Si $controleur='voiture' et $view='list',
 // alors $filepath="/chemin_du_site/view/voiture/list.php"
 
+
     $filepath = File::build_path(array("view", $controller, "$view.php"));
     require $filepath;
     ?>
