68d221419db2b7418bbd07a3fdd5049417b6c38f
guilhem-cros <guilhem.cros01@etu.umontpellier.fr>
2020-12-06 16:28:35 +0100

    répa supp users

--- view/utilisateur/deleted.php
+++ view/utilisateur/deleted.php
@@ -1,4 +1,5 @@
 <?php
 
-echo "L''utilisateur de login " . htmlspecialchars($login) . " a bien été supprimée";
-require (File::build_path(array("view", "pierre", "list.php")));
+echo "<p>L''utilisateur de login " . htmlspecialchars($login) . " a bien été supprimée</p>".
+        '<p><a href="index.php">Retour à la page principale</p>';
+
